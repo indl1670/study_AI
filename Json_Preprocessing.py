7e1e3d24427dbb80277a008e02e12de30d0e65fb
indl1670 <81145399+indl1670@users.noreply.github.com>
2022-02-24 18:23:27 +0900

    Update Json_Preprocessing.py

--- Json_Preprocessing.py
+++ Json_Preprocessing.py
@@ -7,31 +7,24 @@ from collections import OrderedDict
 
 def num1(json_path, out_path):
     json_list = os.listdir(json_path)
-    
+    cat_num = []
+    while True:
+        cat_id = int(input("Enter the category_ids in order(-1 to quit): "))
+        if cat_id == -1:
+            break
+        else:
+            cat_num.append(cat_id)
+    in_name = []
+    print("Enter the category_name that you want!")
+    for i in range(len(cat_num)):
+        print(i+1, ": ", end="\t")
+        in_name.append(input())
+        
     for jl in json_list:
         with open(json_path + jl, "r", encoding="utf-8") as f:
             data = json.load(f)
+                
             
-            cat_num = []
-            cat_name = []
-            for a in data["categories"]:
-                for b in data["annotations"]:
-                    if b["category_id"] == a["id"]:
-                        b["category_id"] = a["name"]
-                        
-                cat_num.append(a["id"])
-                cat_name.append(a["name"])
-                
-            print("\n[Current categories]")
-            for i in range(len(cat_name)):
-                print(cat_num[i], ": ", cat_name[i], " | ", end="")
-            print("")
-                
-            in_name = []
-            print("Enter the category_name that you want!")
-            for i in range(len(cat_name)):
-                print(i+1, ": ", end="\t")
-                in_name.append(input())
             
             for i in range(len(in_name)):
                 for c in data["categories"]:
